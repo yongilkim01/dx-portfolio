a57b1dbd801363e75894bde17c28fdfa26b87cca
yongilkim01 <yongil.k01@gmail.com>
2023-12-30 03:46:18 +0900

    feat(framework): implement winmain function

---
+++ build/CMakeFiles/dx-portfolio.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dx-portfolio.dir/src/main.cc.obj"
+  "CMakeFiles/dx-portfolio.dir/src/main.cc.obj.d"
+  "dx-portfolio.exe"
+  "dx-portfolio.exe.manifest"
+  "dx-portfolio.pdb"
+  "libdx-portfolio.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/dx-portfolio.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/dx-portfolio.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for dx-portfolio.
+# This may be replaced when dependencies are built.
